d5c9caa09dd12616bc5b7b149594a01e2ee50459
Miikka <miikka@mivir.fi>
2017-11-28 10:21:02 +0200

    index.ts: export all entities

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export {Client} from "./client";
 
+export {Asset} from "./entities/Asset";
+export {Entity} from "./entities/Entity";
+export {Match} from "./entities/Match";
+export {Participant} from "./entities/Participant";
 export {Player} from "./entities/Player";
+export {Roster} from "./entities/Roster";
+export {Round} from "./entities/Round";
