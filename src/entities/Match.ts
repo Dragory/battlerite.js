496147b8a8abd853c742799501ac8287520a0f92
Dragory <dragory.up@gmail.com>
2017-11-29 19:48:35 +0200

    Add map and champion data to Match and Participant respectively

--- src/entities/Match.ts
+++ src/entities/Match.ts
@@ -2,6 +2,7 @@ import { Entity } from "./Entity";
 import { Roster } from "./Roster";
 import { Round } from "./Round";
 import { Asset } from "./Asset";
+import { getMapById, IMap } from "../maps";
 
 export class Match extends Entity {
   public createdAt: Date;
@@ -9,6 +10,8 @@ export class Match extends Entity {
   public gameMode: string;
   public patchVersion: string;
   public shardId: string;
+  public stats: any;
+  public map: IMap;
 
   assets: Asset[];
   rosters: Roster[];
@@ -16,6 +19,7 @@ export class Match extends Entity {
 
   _set(key, value) {
     if (key === "createdAt") {
+      // Convert createdAt to a proper date
       value = new Date(value);
     } else if (key === "rounds") {
       // Order rounds by their ordinal
@@ -24,6 +28,9 @@ export class Match extends Entity {
         if (a.ordinal < b.ordinal) return -1;
         return 0;
       });
+    } else if (key === "stats") {
+      // Match stats contain the map ID. Use this to find and set the match's map.
+      super("map", getMapById(value.mapID));
     }
 
     super._set(key, value);
--- src/entities/Participant.ts
+++ src/entities/Participant.ts
@@ -1,20 +1,31 @@
-import {Entity} from "./Entity";
-import {Player} from "./Player";
+import { Entity } from "./Entity";
+import { Player } from "./Player";
+import { getChampionById, IChampion } from "../champions";
 
 export class Participant extends Entity {
-    public actor: string;
-    public stats: {
-        attachment: number;
-        emote: number;
-        mount: number;
-        outfit: number;
-    };
+  public actor: string;
+  public champion: IChampion;
+  public stats: {
+    attachment: number;
+    emote: number;
+    mount: number;
+    outfit: number;
+  };
 
-    player: Player;
+  player: Player;
 
-    _getRelationships() {
-        return {
-            player: Player
-        };
+  _set(key, value) {
+    if (key === "actor") {
+      // Actor = champion ID. Use this to find and set the champion object.
+      super("champion", getChampionById(value));
     }
+
+    super(key, value);
+  }
+
+  _getRelationships() {
+    return {
+      player: Player
+    };
+  }
 }
